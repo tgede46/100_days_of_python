c3b9eeea5ffb6cf6942e036ec9961752b9eef565
tgede46 <gedeonkpara@gmail.com>
2025-01-18 21:33:23 +0000

    almost the end of 10 days

---
+++ day_1_to_10/calculator/operation.py
@@ -0,0 +1,11 @@
+def somme(a, b):
+    return a + b
+
+def defference(a, b):
+    return a - b
+
+def produit(a, b):
+    return a * b
+
+def quotient(a, b):
+    return a / b
---
+++ day_1_to_10/opereations.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return a + b
+
+def soustration(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
